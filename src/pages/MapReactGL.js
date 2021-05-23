e34ba6b5505de5f83c43e2310e402c243d005464
ericYager <eyager@amazon.com>
2021-05-22 22:16:12 -0400

    working example with single overlay somewhere near san fran

--- src/pages/MapReactGL.js
+++ src/pages/MapReactGL.js
@@ -1,27 +1,25 @@
-// ES6
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
+import React, { useState } from 'react';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
  
-const Map = ReactMapboxGl({
-  accessToken: "pk.eyJ1IjoiZXlhZ2VyMTciLCJhIjoiY2tvejVjeXMxMHJuMTJubGQyeXVzMjVpdyJ9.7k0GYXBDfDHC-HPrW3VKcQ"
-});
- 
 export default function MapReactGL() {
-    return(
-        <Map
-        style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{
-            height: '100vh',
-            width: '100vw'
-        }}
-        >
-        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-        </Layer>
-        </Map>
-    )
+
+    const [viewport, setViewport] = React.useState({
+        longitude: -122.45,
+        latitude: 37.78,
+        zoom: 14,
+        mapboxApiAccessToken: "pk.eyJ1IjoiZXlhZ2VyMTciLCJhIjoiY2tvejVjeXMxMHJuMTJubGQyeXVzMjVpdyJ9.7k0GYXBDfDHC-HPrW3VKcQ"
+      });
+      return (
+        <ReactMapGL {...viewport} width="100vw" height="100vh" onViewportChange={setViewport}>
+          <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
+            <div>You are here</div>
+          </Marker>
+        </ReactMapGL>
+      );
 }
