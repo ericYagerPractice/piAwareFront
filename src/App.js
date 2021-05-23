b4936abc37397b9854e78aa2d0a8ad3b101b9961
ericYager <eyager@amazon.com>
2021-05-22 20:53:01 -0400

    added auth in case it's necessary, added mapGL map to /map page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 import Header from './components/Header'
 import './App.css';
+import AWS from 'aws-sdk';
+import awsmobile from './aws-exports';
+
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: awsmobile.aws_cognito_identity_pool_id,
+});
 
 
 function App() {
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,22 +1,39 @@
-import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
-
+import React, { useState, useRef, useEffect } from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import '../staticfiles/Map.css'
+ 
+mapboxgl.accessToken = "pk.eyJ1IjoiZXlhZ2VyMTciLCJhIjoiY2tvejVjeXMxMHJuMTJubGQyeXVzMjVpdyJ9.7k0GYXBDfDHC-HPrW3VKcQ"
 
 export default function Map() {
-  const [viewport, setViewport] = useState({
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+  /*const [viewport, setViewport] = useState({
     width: 1400,
     height: 1400,
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,
     mapboxApiAccessToken: "pk.eyJ1IjoiZXlhZ2VyMTciLCJhIjoiY2tvejVjeXMxMHJuMTJubGQyeXVzMjVpdyJ9.7k0GYXBDfDHC-HPrW3VKcQ"
-  });
+  });*/
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+  })
 
   return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
   );
 }
