3903e2f53a78aad12947c503334e07c8c5b28854
ericYager <eyager@amazon.com>
2021-05-22 22:54:01 -0400

    working example of 1 second refresh of location

--- src/pages/MapReactGL.js
+++ src/pages/MapReactGL.js
@@ -2,24 +2,35 @@ import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import ReactMapGL, {Marker} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
  
 export default function MapReactGL() {
 
-    const [viewport, setViewport] = React.useState({
+    const [viewport, setViewport] = useState({
         longitude: -122.45,
         latitude: 37.78,
-        zoom: 14,
+        zoom: 5,
         mapboxApiAccessToken: "pk.eyJ1IjoiZXlhZ2VyMTciLCJhIjoiY2tvejVjeXMxMHJuMTJubGQyeXVzMjVpdyJ9.7k0GYXBDfDHC-HPrW3VKcQ"
-      });
-      return (
-        <ReactMapGL {...viewport} width="100vw" height="100vh" onViewportChange={setViewport}>
-          <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
-            <div>You are here</div>
-          </Marker>
-        </ReactMapGL>
-      );
+    });
+
+    const [demoAircraftLat, setDemoAircraftLat ] = useState(37.78);
+    const [demoAircraftLon, setDemoAircraftLon] = useState(-122.41)
+
+    useEffect(() => {
+        setInterval(() => {
+            setDemoAircraftLat(demoAircraftLat => demoAircraftLat+.5);
+            setDemoAircraftLon(demoAircraftLon => demoAircraftLon+.5)
+        }, 1000);
+    }, []);
+
+    return (
+    <ReactMapGL {...viewport} width="100vw" height="100vh" onViewportChange={setViewport}>
+        <Marker latitude={demoAircraftLat} longitude={demoAircraftLon} offsetLeft={-20} offsetTop={-10}>
+        <div>You are here</div>
+        </Marker>
+    </ReactMapGL>
+    );
 }
