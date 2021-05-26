65025bc4891ddb1493f30a9deaaa55b7039968fe
ericYager <eyager@amazon.com>
2021-05-26 15:59:22 -0500

    added a test API call in the header function writing to console

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import {listAircraftDatas} from '../graphql/queries'
+import { API, graphqlOperation } from 'aws-amplify'
+
 
 export default function Header() { 
 
-return (
-    <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">PiAware Test View</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="/mapReactGL">Map</Nav.Link>
-                    <Nav.Link href="https://github.com/ericYagerPractice/piAwareFront">Repo</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </>
-);
+    async function setAircraftData(){
+        let aircraftData = await API.graphql(graphqlOperation(listAircraftDatas))
+        console.log(aircraftData)
+    }
+
+    useEffect(() => {
+        setAircraftData()
+    },[])
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">PiAware Test View</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="/mapReactGL">Map</Nav.Link>
+                        <Nav.Link href="https://github.com/ericYagerPractice/piAwareFront">Repo</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    );
 }
+
+
+/*export 
+const listAircraftDatas = 
+query ListAircraftDatas(
+    $filter: ModelAircraftDataFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listAircraftDatas(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        data
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;*/
