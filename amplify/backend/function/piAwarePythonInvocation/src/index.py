54e63379813a061a611945248d3a880ccd9f7e22
ericYager <eyager@amazon.com>
2021-06-10 09:37:49 -0500

    updated python lambda to handle aircraft data

--- amplify/backend/function/piAwarePythonInvocation/src/index.py
+++ amplify/backend/function/piAwarePythonInvocation/src/index.py
@@ -1,15 +1,86 @@
 import json
+from boto3 import Session as AWSSession
+from requests_aws4auth import AWS4Auth
+from gql import gql
+from gql.client import Client
+from gql.transport.requests import RequestsHTTPTransport
+
+def aircraftDataFunction(client, event):
+    writeAircraftData = """
+        mutation CreateAircraftData(
+            $input: CreateAircraftDataInput!
+            $condition: ModelAircraftDataConditionInput
+        ) {
+        createAircraftData(input: $input, condition: $condition) {
+            id
+            data
+        }
+    }
+    """
+
+    params = {
+        "input" : {"data": event}
+    }
+
+    queryOperation = client.execute(
+        gql(writeAircraftData),
+        variable_values = json.dumps(params)
+    )
+
+    return queryOperation
+
+def clientConfiguration():
+    configDetails = {
+        'keyID': 'AKIA36EJB4OYMJ37Q46H',
+        'key': 'Bd3EW9liPdWQPvu/bAPed5r3kIcbOK8azk62yI5d',
+        'region': 'us-east-1',
+        'endpoint': 'https://yoob6l7g3jbhne3esl2hhjkemy.appsync-api.us-east-1.amazonaws.com/graphql'
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+      
+    aws = AWSSession(aws_access_key_id=configDetails['keyID'],
+                     aws_secret_access_key=configDetails['key'],
+                     region_name=configDetails['region'])
+
+    credentials = aws.get_credentials().get_frozen_credentials()
+
+    auth = AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key,
+        aws.region_name,
+        'appsync',
+        session_token=credentials.token,
+    )
+
+    transport = RequestsHTTPTransport(url=configDetails['endpoint'],
+                                      headers=headers,
+                                      auth=auth)
+
+    client = Client(transport=transport,
+                    fetch_schema_from_transport=True)
+                    
+    return client
+
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+    print('received event:')
+    print("event: ",event)
+    print("context", context)
+
+    client = clientConfiguration()
+
+    writeData = aircraftDataFunction(client, event)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(writeData)
   }
